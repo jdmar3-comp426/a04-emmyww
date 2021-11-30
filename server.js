a50bc3920d301063000215c95180ef02d40e5959
Emmy Whitford <emmyw@live.unc.edu>
2021-11-30 17:08:30 -0500

    trying the rest

--- server.js
+++ server.js
@@ -41,19 +41,28 @@ app.get("/app/users", (req, res) => {
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users:id", (req, res) => {
-	const stmt = db.prepare("SELECT * FROM user.info WHERE id = ?").get(req.params.id); //??
+	const stmt = db.prepare("SELECT * FROM user.info WHERE id = ?")
+	const info = stmt.get(req.params.id); //??
 	res.status(200).json(stmt);
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?, user), pass = COALESCE(?, pass) WHERE id = ?");
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id);
 	res.status(200).json({
 		message: `1 record updated: ID ${req.params.id} (200)`,
 	});
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
+app.delete("/app/delete/user:id", (req, res) => {
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id=?");
+	const info = stmt.run(req.params.id);
+	res.status(200).json({
+		message: `1 record deleted: ID ${req.params.id} (200)`,
+	})
+});
 
 // Default response for any other request
 app.use(function(req, res){
